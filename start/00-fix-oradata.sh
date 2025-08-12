51259ec09513eabbecee375a032bf9dbf6b72a06
Fernando de Lima <fernando.lima@ambevtech.com.br>
2025-08-12 10:54:40 -0300

    chore(start): make fix-oradata executable

--- start/00-fix-oradata.sh
+++ start/00-fix-oradata.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
-# cria as pastas que o DB espera e ajusta dono para o usuário 'oracle' (UID=54321)
+# garante as pastas esperadas e o owner correto
 mkdir -p /opt/oracle/oradata/FREE /opt/oracle/oradata/FREEPDB1
 chown -R 54321:54321 /opt/oracle/oradata
